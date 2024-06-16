ec4e9d0183e732a7025033d7793c68f1141d81ce
fabiosimplis <fabio.simplis@gmail.com>
2024-06-16 15:24:05 -0300

    Controller

--- src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDto.java
+++ src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDto.java
@@ -4,5 +4,5 @@ import br.com.alurafood.pagamentos.model.Status;
 
 import java.math.BigDecimal;
 
-public record PagamentoDto(BigDecimal valor, String nome, String numero, String exp, String codigo, Status status, Long formaDePagamentoId, Long PedidoId) {
+public record PagamentoDto(Long id, BigDecimal valor, String nome, String numero, String exp, String codigo, Status status, Long formaDePagamentoId, Long PedidoId) {
 }
